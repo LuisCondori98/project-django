85cb670faebe780a49a48a88a89cf3a897343448
luiscondori98 <lcondori11@hotmail.com>
2022-12-16 15:46:42 -0500

    añadiendo urls

--- tiendaOnline/appcoder/views.py
+++ tiendaOnline/appcoder/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+def inicio(request):
+    return render(request, "appcoder/index.html")
+
+def cursos(request):
+    return render(request, "appcoder/cursos.html")
+
+def estudiantes(request):
+    return render(request, "appcoder/estudiantes.html")
+
+def profesores(request):
+    return render(request, "appcoder/profesores.html")
+
+def entregables(request):
+    return render(request, "appcoder/entregables.html")
 
 # Create your views here.
--- tiendaOnline/tiendaOnline/urls.py
+++ tiendaOnline/tiendaOnline/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from tiendaOnline.views import *
 
 urlpatterns = [
@@ -25,4 +25,5 @@ urlpatterns = [
     path('Inicio/', plantilla),
     path('alumnos', vista_listado_alumnos),
     path('alumnos2', vista_listado_alumnos2),
+    path("coder/", include("appcoder.urls")),
 ]
